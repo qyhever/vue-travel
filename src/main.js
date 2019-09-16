62156aa6961a52046ac3417d621c2396ddc21ac2
query <query@163.com>
2019-09-16 16:53:36 +0800

    2019/9/16 16:53

--- src/main.js
+++ src/main.js
@@ -25,6 +25,12 @@ Raven.config('https://3695b4a5dfde44599bd5bdf2775c735e@sentry.io/1444638', {
   debug: true
 }).addPlugin(RavenVue, Vue).install()
 
+try {
+  const c = a + b
+} catch (err) {
+  Sentry.captureException(err)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
